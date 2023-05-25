bbfd11d062506fd90fedaac9c91e24d86b9358d4
harkinder-dark <victorvaddely@gmail.com>
2023-05-25 11:54:36 +0000

    add encrypt_password file

---
+++ 0x00-personal_data/3-main.py
@@ -0,0 +1,9 @@
+"""
+Main file
+"""
+
+hash_password = __import__('encrypt_password').hash_password
+
+password = "MyAmazingPassw0rd"
+print(hash_password(password))
+print(hash_password(password))
--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -2,3 +2,14 @@
 """
 logging learning
 """
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """hashage"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """validate"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)

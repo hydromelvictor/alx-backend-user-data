5a02b6bbb818d2eabbc2ae1ce1736e6bfa3d16cb
harkinder-dark <victorvaddely@gmail.com>
2023-05-30 10:30:28 +0000

    update basic_auth file

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,10 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """basic auth"""
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """extract base64 authorization header"""
+        if authorization_header and type(authorization_header) is str:
+            if authorization_header.split(' ')[0] == 'Basic':
+                return authorization_header.split(' ')[1]
+        return None

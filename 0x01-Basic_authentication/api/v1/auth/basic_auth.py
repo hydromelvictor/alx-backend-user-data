4cff0fb5b01c5134f8c00e428b18a688ade68d6c
harkinder-dark <victorvaddely@gmail.com>
2023-05-30 11:27:24 +0000

    update basic_auth file

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -42,11 +42,4 @@ class BasicAuth(Auth):
     def user_object_from_credentials(
             self, user_email: str, user_pwd: str) -> TypeVar('User'):
         """user object from credentials"""
-        if not user_email or type(user_email) is not str:
-            return None
-        if not user_pwd or type(user_pwd) is not str:
-            return None
-        elt = User.search({'email': user_email})
-        if User.count() and elt:
-            return elt[0]
         return None

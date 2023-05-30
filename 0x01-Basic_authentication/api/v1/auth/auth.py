4b7872be9604cea5837ad7aee4ce552af35d7b29
harkinder-dark <victorvaddely@gmail.com>
2023-05-30 09:51:31 +0000

    update auth.py file

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,6 +9,19 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require_auth"""
+        exclus = []
+        path_ = None
+        if excluded_paths:
+            for s in excluded_paths:
+                if s[-1] == '*':
+                    s = s[:len(s) - 1]
+                exclus.append(s)
+        if path:
+            path_ = path + '/' if path[-1] != '/' else path[:len(path) - 1]
+        if path is None or \
+            (path not in exclus and path_ not in exclus) or \
+                excluded_paths is None or len(excluded_paths) == 0:
+            return True
         return None
 
     def authorization_header(self, request=None) -> str:

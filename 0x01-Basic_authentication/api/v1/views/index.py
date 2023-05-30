dd65f6fe39057104815b211347055f20312a21fd
harkinder-dark <victorvaddely@gmail.com>
2023-05-30 09:24:42 +0000

    app.py index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', methods['Get'], strict_slashes=False)
-def forbidden() -> str:
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
     """GET /api/v1/forbidden"""
     abort(403)

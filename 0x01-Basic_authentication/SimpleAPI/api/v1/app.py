6a7131fe4752cb99301b8e2a73d53893433eadac
harkinder-dark <victorvaddely@gmail.com>
2023-05-30 04:10:59 +0000

    add new directory for authentication learn

---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+from os import getenv
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+
+if getenv('AUTH_TYPE'):
+    auth_type = getenv('AUTH_TYPE')
+    from api.v1.auth.auth import Auth
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth() if auth_type == 'basic_auth' else Auth()
+
+
+def req_before():
+    """before request"""
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if not auth or auth.require_auth(request.path, excluded_paths):
+        pass
+    if auth.authorization_header(request) == None:
+        abort(404)
+    if auth.current_user(request) == None:
+        abort(403)
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def error_unauthorized(error) -> str:
+    """request unauthorized"""
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def error_forbidden(error) -> str:
+    """forbidden error"""
+    return jsonify({"error": "Forbidden"}), 403
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""Create a folder api/v1/auth
+Create an empty file api/v1/auth/__init__.py
+Create the class Auth:
+in the file api/v1/auth/auth.py
+"""
+from flask import request
+
+
+class Auth:
+    """Authorization class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        description
+        ===========
+
+        parameters
+        ===========
+        path: str
+        excluded_paths: List[str]
+
+        __return__: boolean
+        """
+        if excluded_paths:
+            excluded_all = [i[:len(i) - 1] for i in excluded_paths if i[-1] == '*']
+        path0 = path + '/' if path[-1] != '/' else path[:len(path) - 1]
+        if path is None or (path not in excluded_all and 
+                            path0 not in excluded_all) \
+                                or excluded_paths is None or \
+                                    len(excluded_paths) == 0:
+            return True
+        return False
+    
+    def authorization_header(self, request=None) -> str:
+        """
+        description
+        ============
+
+        parameters
+        ============
+        request=None
+
+        __return__: None
+        """
+        return request.headers.get('Authorization', None)
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        description
+        ============
+
+        parameters
+        ============
+        request=None
+
+        __return__: TypeVar('User')
+        """
+        return None

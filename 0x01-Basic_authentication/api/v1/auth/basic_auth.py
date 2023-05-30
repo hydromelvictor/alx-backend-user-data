2dc37310a02fa10839d23da94dda517fa17e0249
harkinder-dark <victorvaddely@gmail.com>
2023-05-30 10:36:32 +0000

    update app.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """class basicauth"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -11,4 +12,16 @@ class BasicAuth(Auth):
         if authorization_header and type(authorization_header) is str:
             if authorization_header.split(' ')[0] == 'Basic':
                 return authorization_header.split(' ')[1]
+        return None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """decode base64 authorization header"""
+        if base64_authorization_header and \
+                type(base64_authorization_header) is str:
+            try:
+                return base64.b64decode(
+                    base64_authorization_header).decode('utf-8')
+            except Exception:
+                return None
         return None

743fb82e68f20cd41eabc8cc9a4b6e681f0943ea
harkinder-dark <victorvaddely@gmail.com>
2023-06-01 07:46:54 +0000

    add new directory

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""session authentication"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """inheritance by auth"""
+    pass
--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -30,8 +30,7 @@ def view_one_user(user_id: str = None) -> str:
     if user_id is 'me':
         if request.current_user is None:
             abort(404)
-        else:
-            return jsonify(request.current_user.to_json())
+        return jsonify(request.current_user.to_json())
     user = User.get(user_id)
     if user is None:
         abort(404)

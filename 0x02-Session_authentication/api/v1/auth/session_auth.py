dff722eadf24603802d363f6217e7a0fdb394288
harkinder-dark <victorvaddely@gmail.com>
2023-06-01 08:03:52 +0000

    update session_auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,26 @@
 """session authentication"""
 
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """inheritance by auth"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        description
+        ==============
+
+        parameters
+        ===========
+        user_id : str
+
+        __return__  : str
+        ===========
+        """
+        if user_id is None and type(user_id) is not str:
+            return None
+        self.user_id_by_session_id[str(uuid4())] = user_id
+        return self.user_id_by_session_id

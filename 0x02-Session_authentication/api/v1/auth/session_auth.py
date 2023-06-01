48652b716e3008b9230d58fc9a1dd9fb66ea86e3
harkinder-dark <victorvaddely@gmail.com>
2023-06-01 08:22:01 +0000

    update session_auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -26,3 +26,19 @@ class SessionAuth(Auth):
         key = str(uuid4())
         self.user_id_by_session_id[key] = user_id
         return key
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        description
+        ==============
+
+        parameters
+        ===========
+        session_id : str
+
+        __return__  : str
+        ===========
+        """
+        if session_id is None and type(session_id) is not str:
+            return None
+        return self.user_id_by_session_id.get(session_id)

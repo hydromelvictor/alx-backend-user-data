20ea27545cd0f67fecfa029a14de5222934ef6b6
harkinder-dark <victorvaddely@gmail.com>
2023-06-01 21:08:41 +0000

    update task 0

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -27,7 +27,7 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
-    if user_id is 'me':
+    if user_id == 'me':
         if request.current_user is None:
             abort(404)
         return jsonify(request.current_user.to_json())

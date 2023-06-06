3d9634a3eb6e268d0c97ba000d45bb30bf9b1b9b
harkinder-dark <victorvaddely@gmail.com>
2023-06-06 11:29:46 +0000

    add new directory name is 0x03-user_authantication_service

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -36,9 +36,10 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """new user"""
+        session = self._session
         user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
+        session.add(user)
+        session.commit()
         return user
 
     def find_user_by(self, **kwargs) -> User:
